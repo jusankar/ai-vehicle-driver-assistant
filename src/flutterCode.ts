135c41884e09ba8a1ec2e3da37568edfbc36af45
Udayasankar J <jusankar@gmail.com>
2026-08-01 19:15:38 +0530

    build(android): upgrade gradle and kotlin versions
    Update Android Gradle Plugin to 8.7.3 and Kotlin to 2.0.21. Removed redundant compileSdkVersion overrides in build.gradle.

--- src/flutterCode.ts
+++ src/flutterCode.ts
@@ -4820,8 +4820,8 @@ distributionUrl=https\\://services.gradle.org/distributions/gradle-8.14-all.zip
 
 plugins {
     id "dev.flutter.flutter-plugin-loader" version "1.0.0"
-    id "com.android.application" version "8.7.0" apply false
-    id "org.jetbrains.kotlin.android" version "1.9.24" apply false
+    id "com.android.application" version "8.7.3" apply false
+    id "org.jetbrains.kotlin.android" version "2.0.21" apply false
 }
 
 include ":app"
@@ -4851,16 +4851,6 @@ subprojects {
 }
 subprojects {
     project.evaluationDependsOn(':app')
-}
-
-subprojects {
-    afterEvaluate { p ->
-        if (p.hasProperty('android')) {
-            p.android {
-                compileSdkVersion 36
-            }
-        }
-    }
 }
 
 tasks.register("clean", Delete) {
